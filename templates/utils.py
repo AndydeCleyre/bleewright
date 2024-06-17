a16996067f88fdd823e44539980a0fde7d77b225
Andy Kluger <AndyKluger@gmail.com>
2024-06-17 16:10:01 -0400

    include variant templates

--- templates/utils.py
+++ templates/utils.py
@@ -1,3 +1,40 @@
+from json import dumps
+from plumbum.cmd import pastel
+
+
+def prot(color: str) -> str:
+    return (pastel['colorblind', 'prot', color] | pastel['format'])().strip()
+
+
+def deuter(color: str) -> str:
+    return (pastel['colorblind', 'deuter', color] | pastel['format'])().strip()
+
+
+def trit(color: str) -> str:
+    return (pastel['colorblind', 'trit', color] | pastel['format'])().strip()
+
+
+# def rot180(color: str) -> str:
+    # HMMM:
+    # - pastel complement
+    # - pastel rotate ANGLE
+    # - pastel . . . negate?
+    # return (pastel['rotate', '180', color] | pastel['format'])().strip()
+
+
+# def variant_json(meta: dict, colors: dict, alter, suffix: str) -> str:
+def variant_json(meta: dict, colors: dict, alter) -> str:
+    suffix = alter.__name__.title()
+    data = {}
+    data['meta'] = meta
+    data['meta']['name'] = f"{meta['name']}{suffix}"
+    data['colors'] = {}
+    data['colors']['bg'] = { k: alter(v) for k, v in colors['bg'].items() }
+    data['colors']['fg'] = { k: alter(v) for k, v in colors['fg'].items() }
+
+    return dumps(data)
+
+
 def hex_to_rgb(hexcode: str) -> str:
     return ','.join(
         str(int(hexcode.lstrip('#')[i : i + 2], 16))
